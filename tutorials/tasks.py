f11687e7a28b61af40fac8f8a7a30528463198d0
Luca Rezzonico <lucarezzonico97@gmail.com>
2023-06-14 13:47:16 +0200

    make all models work again

--- tutorials/tasks.py
+++ tutorials/tasks.py
@@ -85,18 +85,8 @@ class Tasks():
         
             # Simulation folder for visualization in nuBoard
             simulation_folder = cfg.output_dir
-            
-        # self.plt_radar_data()
         
         return simulation_folder
-    
-    # def plt_radar_data(self):
-    #     self._metric_statistics_dataframes = self._read_metric_parquet_files(
-    #         metric_save_path=self._metric_save_path,
-    #         metric_reader=metric_statistics_reader,
-    #     )
-        
-        
 
 
     def open_nuboard(self, cfg: DictConfig, simulation_folders: List[str]) -> None:
